2836ba46045936f901032caa80637d8d44941eb5
Johannes Thorbergsson <johannes.thorbergsson@hotmail.de>
2023-03-17 00:22:01 +0100

    Add @Id annotation for User model

--- backend/src/main/java/com/github/johannesthorbergsson/backend/security/MongoUser.java
+++ backend/src/main/java/com/github/johannesthorbergsson/backend/security/MongoUser.java
@@ -1,8 +1,10 @@
 package com.github.johannesthorbergsson.backend.security;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("user")
 public record MongoUser(
+    @Id
     String id,
     String username,
     String password,
