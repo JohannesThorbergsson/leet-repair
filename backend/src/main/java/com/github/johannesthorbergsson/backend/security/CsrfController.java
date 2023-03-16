b0eafd55284bddcd8a24dd9dee4a01ee9ff85ed7
johannesthorbergsson <johannes.thorbergsson@hotmail.de>
2023-03-16 13:02:44 +0100

    Add CsrfController.java and set MONGODB_URI env

---
+++ backend/src/main/java/com/github/johannesthorbergsson/backend/security/CsrfController.java
@@ -0,0 +1,14 @@
+package com.github.johannesthorbergsson.backend.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/csrf")
+public class CsrfController {
+    @GetMapping
+    public String getCsrf() {
+        return "CSRF Ok";
+    }
+}
