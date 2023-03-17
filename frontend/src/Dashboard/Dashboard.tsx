bae54ed4cb45966e1cdea0326c94f9b2eb8b6b54
johannesthorbergsson <johannes.thorbergsson@hotmail.de>
2023-03-17 09:12:56 +0100

    Delete Dashboard component

--- frontend/src/Dashboard/Dashboard.tsx
+++
@@ -1,8 +0,0 @@
-import useAuth from "../Hooks/useAuth";
-
-export default function Dashboard() {
-    const user = useAuth(true)
-    return (!user? <></>:
-        <h1>Dashboard goes here</h1>
-    )
-}
