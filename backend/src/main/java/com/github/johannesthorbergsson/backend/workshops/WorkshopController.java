c2062dacb79ac7c03f249685fe1926e458733909
johannesthorbergsson <johannes.thorbergsson@hotmail.de>
2023-03-17 17:38:31 +0100

    Add WorkshopController

---
+++ backend/src/main/java/com/github/johannesthorbergsson/backend/workshops/WorkshopController.java
@@ -0,0 +1,20 @@
+package com.github.johannesthorbergsson.backend.workshops;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/workshops/")
+@RequiredArgsConstructor
+public class WorkshopController {
+    private final WorkshopService workshopService;
+
+    @GetMapping
+    public List<Workshop> getAllWorkshops() {
+        return workshopService.getAllWorkshops();
+    }
+}
