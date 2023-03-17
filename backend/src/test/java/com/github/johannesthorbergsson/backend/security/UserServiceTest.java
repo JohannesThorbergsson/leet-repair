b69f757d2f8ef173954ff43134909f960a3d4454
johannesthorbergsson <johannes.thorbergsson@hotmail.de>
2023-03-17 12:43:24 +0100

    Add unit tests for createUser

--- backend/src/test/java/com/github/johannesthorbergsson/backend/security/UserServiceTest.java
+++ backend/src/test/java/com/github/johannesthorbergsson/backend/security/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.github.johannesthorbergsson.backend.security;
 
+import com.github.johannesthorbergsson.backend.id.IdService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -7,16 +8,18 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class UserServiceTest {
 
@@ -25,15 +28,18 @@ class UserServiceTest {
     Principal principal = mock(Principal.class);
     MongoUser mongoUser = new MongoUser("1", "name", "11", "BASIC");
     UserResponse userResponse = new UserResponse(mongoUser.id(), mongoUser.username(), mongoUser.role());
+    UserRequest userRequest = new UserRequest(mongoUser.username(), mongoUser.password());
+    IdService idService = mock(IdService.class);
+    PasswordEncoder passwordEncoder = mock(PasswordEncoder.class);
 
     @BeforeEach
     void setUp() {
         when(userRepository.findByUsername(mongoUser.username())).thenReturn(Optional.ofNullable(mongoUser));
-        userService = new UserService(userRepository);
+        when(idService.generateId()).thenReturn(mongoUser.id());
+        userService = new UserService(userRepository, idService, passwordEncoder);
 
     }
     @Test
-    @DirtiesContext
     void loadUserByUsername_whenUserExists_thenReturnUserResponse() {
         //GIVEN
         GrantedAuthority grantedAuthority = () -> "ROLE_" + mongoUser.role();
@@ -45,7 +51,6 @@ class UserServiceTest {
         assertEquals(expected, actual);
     }
     @Test
-    @DirtiesContext
     void loadUserByUsername_whenUserNotFound_thenThrowException(){
         //GIVEN
         UsernameNotFoundException expected = new UsernameNotFoundException("User not found");
@@ -57,7 +62,6 @@ class UserServiceTest {
     }
 
     @Test
-    @DirtiesContext
     void getCurrentUser_whenValidUser_thenReturnUser() {
         //GIVEN
         when(principal.getName()).thenReturn(mongoUser.username());
@@ -65,6 +69,8 @@ class UserServiceTest {
         //WHEN
         UserResponse actual = userService.getCurrentUser(principal);
         //THEN
+        verify(principal).getName();
+        verify(userRepository).findByUsername(mongoUser.username());
         assertEquals(expected, actual);
     }
 
@@ -76,7 +82,52 @@ class UserServiceTest {
         //WHEN
         ResponseStatusException actual = assertThrows(ResponseStatusException.class, () -> userService.getCurrentUser(principal));
         //THEN
+        verify(principal).getName();
+        verify(userRepository).findByUsername("invalid");
         assertEquals(expected.getClass(), actual.getClass());
         assertEquals(expected.getMessage(), actual.getMessage());
+    }
+    @Test
+    void createUser_whenUserCreated_thenReturnUserResponse() {
+        //GIVEN
+        when(userRepository.save(mongoUser)).thenReturn(mongoUser);
+        when(passwordEncoder.encode(mongoUser.password())).thenReturn("11");
+        UserResponse expected = userResponse;
+        //WHEN
+        UserResponse actual = userService.createUser(userRequest);
+        //THEN
+        verify(userRepository).save(mongoUser);
+        assertEquals(expected, actual);
+    }
+    @Test
+    void createUser_whenUsernameNotUnique_thenThrowResponseStatusException() {
+        //GIVEN
+        when(userRepository.existsByUsername(mongoUser.username())).thenReturn(true);
+        HttpStatus expected = HttpStatus.CONFLICT;
+        //WHEN
+        ResponseStatusException actual = assertThrows(ResponseStatusException.class, () -> userService.createUser(userRequest));
+        //THEN
+        verify(userRepository).existsByUsername(mongoUser.username());
+        assertEquals(expected, actual.getStatusCode());
+    }
+    @Test
+    void createUser_whenUsernameEmpty_thenThrowResponseStatusException() {
+        //GIVEN
+        UserRequest noUsername = new UserRequest("", "1");
+        HttpStatus expected = HttpStatus.BAD_REQUEST;
+        //WHEN
+        ResponseStatusException actual = assertThrows(ResponseStatusException.class, () -> userService.createUser(noUsername));
+        //THEN
+        assertEquals(expected, actual.getStatusCode());
+    }
+    @Test
+    void createUser_whenPasswordEmpty_thenThrowResponseStatusException() {
+        //GIVEN
+        UserRequest noPassword = new UserRequest("1", "");
+        HttpStatus expected = HttpStatus.BAD_REQUEST;
+        //WHEN
+        ResponseStatusException actual = assertThrows(ResponseStatusException.class, () -> userService.createUser(noPassword));
+        //THEN
+        assertEquals(expected, actual.getStatusCode());
     }
 }
