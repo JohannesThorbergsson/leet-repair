9192bd08c693dfb119a398a46039b7d141d9561d
johannesthorbergsson <johannes.thorbergsson@hotmail.de>
2023-03-17 15:49:04 +0100

    Merged with master

--- backend/src/main/java/com/github/johannesthorbergsson/backend/security/UserController.java
+++ backend/src/main/java/com/github/johannesthorbergsson/backend/security/UserController.java
@@ -3,10 +3,7 @@ package com.github.johannesthorbergsson.backend.security;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
