4595198634674a7661077cae125ac1fa504bed0f
johannesthorbergsson <johannes.thorbergsson@hotmail.de>
2023-03-17 14:24:20 +0100

    Add Log out button to frontend

---
+++ frontend/src/LogOutButton/LogOutButton.tsx
@@ -0,0 +1,13 @@
+import useAuth from "../Hooks/useAuth";
+import axios from "axios";
+
+export default function LogOutButton(){
+    const user = useAuth(false)
+    function handleLogOut() {
+        axios.post('/api/users/logout')
+            .then(() => window.location.href = '/login')
+    }
+    return (
+        user? <button onClick={handleLogOut}>Log out</button>: <></>
+    )
+}
--- frontend/src/Login/Login.tsx
+++ frontend/src/Login/Login.tsx
@@ -1,6 +1,7 @@
 import {ChangeEvent, FormEvent, useState} from "react";
 import axios from 'axios'
 import {useNavigate} from "react-router-dom";
+import LogOutButton from "../LogOutButton/LogOutButton";
 
 
 export default function Login() {
@@ -26,17 +27,20 @@ export default function Login() {
             .catch(error => console.log(error))
     }
     return (
-        <form className={"login__form"} onSubmit={handleSubmit}>
-            <h1>Log in</h1>
-            <label>
-                <span>Username:</span>
-                <input type={"text"} value={username} onChange={handleUsernameChange}/>
-            </label>
-            <label>
-                <span>Password:</span>
-                <input type={"password"} value={password} onChange={handlePasswordChange}/>
-            </label>
-            <button type={"submit"}>Log in</button>
-        </form>
+        <>
+            <form className={"login__form"} onSubmit={handleSubmit}>
+                <h1>Log in</h1>
+                <label>
+                    <span>Username:</span>
+                    <input type={"text"} value={username} onChange={handleUsernameChange}/>
+                </label>
+                <label>
+                    <span>Password:</span>
+                    <input type={"password"} value={password} onChange={handlePasswordChange}/>
+                </label>
+                <button type={"submit"}>Log in</button>
+            </form>
+            <LogOutButton/>
+        </>
     )
 }
