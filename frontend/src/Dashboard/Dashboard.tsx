af2720c607f629e31513b8e74b27412cd2175351
JohannesThorbergsson <johannes.thorbergsson@hotmail.de>
2023-03-18 19:11:04 +0100

    Add Workshop.ts model

--- frontend/src/Dashboard/Dashboard.tsx
+++ frontend/src/Dashboard/Dashboard.tsx
@@ -1,12 +1,23 @@
 import Layout from "../Layout/Layout";
 import useAuth from "../Hooks/useAuth";
+import {useState} from "react";
+import axios from "axios";
 
 export default function Dashboard() {
     const user = useAuth(true)
+    const [search, setSearch] = useState(false)
+    function searchHandler() {
+        setSearch(true)
+        axios.get("/api/workshops/")
+            .then(r => r.data)
+    }
     return (user &&
         <Layout>
             <input type={"text"}/>
-            <button>Search</button>
+            <button onClick={searchHandler}>Search</button>
+            {!search?
+            <h1>Dashboard</h1>:
+            <div>Search results</div> }
         </Layout>
     )
 }
