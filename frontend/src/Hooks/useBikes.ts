45c24a0d3d0d563f560cbbd40ad8109560cdc50b
johannesthorbergsson <johannes.thorbergsson@hotmail.de>
2023-03-20 17:01:23 +0100

    Create useBikes hook

---
+++ frontend/src/Hooks/useBikes.ts
@@ -0,0 +1,15 @@
+import {useEffect, useState} from "react";
+import {Bike} from "../model/Bike";
+import axios from "axios";
+
+export default function useBikes(){
+    const [bikes, setBikes] = useState<Bike[]>([])
+
+    useEffect(fetchBikes, [])
+
+    function fetchBikes(){
+        axios.get("api/bikes")
+            .then(r => setBikes(r.data))
+    }
+    return {bikes}
+}
