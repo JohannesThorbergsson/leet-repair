6b30144257f6f324d3a3a6ff535537d0f29058f8
johannesthorbergsson <johannes.thorbergsson@hotmail.de>
2023-03-20 13:02:02 +0100

    Add BikeControllerTest

---
+++ backend/src/main/java/com/github/johannesthorbergsson/backend/bikes/BikeController.java
@@ -0,0 +1,20 @@
+package com.github.johannesthorbergsson.backend.bikes;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/bikes")
+@RequiredArgsConstructor
+public class BikeController {
+    private final BikeService bikeService;
+
+    @GetMapping
+    public List<Bike> getAllBikes(){
+        return bikeService.getAllBikes();
+    }
+}
