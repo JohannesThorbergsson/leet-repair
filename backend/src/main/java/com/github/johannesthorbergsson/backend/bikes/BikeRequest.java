984baf8df7d9bdffbaec5976dd7ce4298395336e
johannesthorbergsson <johannes.thorbergsson@hotmail.de>
2023-03-21 18:04:36 +0100

    Add BikeRequest Model

---
+++ backend/src/main/java/com/github/johannesthorbergsson/backend/bikes/BikeRequest.java
@@ -0,0 +1,11 @@
+package com.github.johannesthorbergsson.backend.bikes;
+
+import java.util.List;
+
+public record BikeRequest(
+        String modelName,
+        int mileage,
+        List<Component> components
+
+){
+}
