bf25b75969aff1568aa6bb7eb180e1697d51ff4e
johannesthorbergsson <johannes.thorbergsson@hotmail.de>
2023-03-21 22:05:31 +0100

    Style BikeCard.tsx

--- frontend/src/Component/BikeCard/BikeCard.tsx
+++ frontend/src/Component/BikeCard/BikeCard.tsx
@@ -1,7 +1,17 @@
 import {Bike} from "../../model/Bike";
 import React from "react";
-import {Card, CardContent, Typography} from "@mui/material";
-import BikeComponent from "./BikeComponent";
+import {
+    Card,
+    CardContent,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography
+} from "@mui/material";
 
 type Props = {
     bike: Bike
@@ -20,9 +30,31 @@ export default function BikeCard(props: Props) {
                 <Typography variant="subtitle1" component="h6" fontWeight={"medium"}>
                     Installed Components:
                 </Typography>
-                {props.bike.components.map(c =>
-                    <BikeComponent key={c.type} component={c}/>
-                )}
+                <TableContainer component={Paper}>
+                    <Table sx={{ }} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Component</TableCell>
+                                <TableCell align="left">Model</TableCell>
+                                <TableCell align="right">Age (km)</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {props.bike.components.map((component) => (
+                                <TableRow
+                                    key={component.type}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell component="th" scope="row">
+                                        {component.category}
+                                    </TableCell>
+                                    <TableCell align="left">{component.type}</TableCell>
+                                    <TableCell align="right">{component.ageKm}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </CardContent>
         </React.Fragment>
     );
