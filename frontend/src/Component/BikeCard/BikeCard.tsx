55daa90617eace52c185242c391244701edacadc
johannesthorbergsson <johannes.thorbergsson@hotmail.de>
2023-03-21 18:40:22 +0100

    Restyle BikeCard.tsx with MUI

--- frontend/src/Component/BikeCard/BikeCard.tsx
+++ frontend/src/Component/BikeCard/BikeCard.tsx
@@ -1,15 +1,36 @@
 import {Bike} from "../../model/Bike";
+import React from "react";
+import {Card, CardContent, Typography} from "@mui/material";
+import BikeComponent from "./BikeComponent";
 
 type Props = {
     bike: Bike
 }
 
 export default function BikeCard(props: Props) {
+    const card = (
+        <React.Fragment>
+            <CardContent>
+                <Typography variant="h6" fontWeight={"medium"}>
+                    {props.bike.modelName}
+                </Typography>
+                <Typography variant="subtitle2" component="h6" fontWeight={"small"}>
+                    Mileage: {props.bike.mileage} km
+                </Typography>
+                <Typography variant="subtitle1" component="h6" fontWeight={"medium"}>
+                    Installed Components:
+                </Typography>
+                {props.bike.components.map(c =>
+                    <BikeComponent key={c.type} component={c}/>
+                )}
+            </CardContent>
+        </React.Fragment>
+    );
     return (
         <div>
-            <h2>{props.bike.modelName}</h2>
-            <h3>{props.bike.mileage}</h3>
-            {props.bike.components.map(c => <p key={c.type}>{c.type}</p>)}
+            <Card variant={"outlined"} sx={{
+                m: 2
+            }}>{card}</Card>
         </div>
     )
 }
---
+++ frontend/src/Component/BikeCard/BikeComponent.tsx
@@ -0,0 +1,26 @@
+import {Container, Typography} from "@mui/material";
+import {Component} from "../../model/Component";
+
+type Props = {
+    component: Component
+}
+export default function BikeComponent(props: Props){
+    return (
+        <Container sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between'
+
+        }}>
+            <Typography variant={"body1"}>
+                {props.component.category}
+            </Typography>
+            <Typography variant="body1">
+                Model: {props.component.type}
+            </Typography>
+            <Typography variant={"body1"}>
+                {props.component.ageKm} km
+            </Typography>
+        </Container>
+    )
+}
