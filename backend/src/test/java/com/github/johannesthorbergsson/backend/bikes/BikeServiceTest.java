d373c0bfb7f13645be905e65cbb0c9e920e7244b
johannesthorbergsson <johannes.thorbergsson@hotmail.de>
2023-03-21 18:05:17 +0100

    Add unit test for addBike

--- backend/src/test/java/com/github/johannesthorbergsson/backend/bikes/BikeServiceTest.java
+++ backend/src/test/java/com/github/johannesthorbergsson/backend/bikes/BikeServiceTest.java
@@ -1,5 +1,6 @@
 package com.github.johannesthorbergsson.backend.bikes;
 
+import com.github.johannesthorbergsson.backend.id.IdService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,13 +14,14 @@ import static org.mockito.Mockito.*;
 class BikeServiceTest {
     BikeService bikeService;
     BikeRepository bikeRepository = mock(BikeRepository.class);
+    IdService idService = mock(IdService.class);
     Principal principal = mock(Principal.class);
     Component tyre = new Component("tyre", "Pirelli", 1337);
     Bike testBike = new Bike("1", "MegaBike9000", "steven", 1337, List.of(tyre));
 
     @BeforeEach
     void setUp(){
-        bikeService = new BikeService(bikeRepository);
+        bikeService = new BikeService(bikeRepository, idService);
     }
     @Test
     void getAllBikes_whenBikesMatchPrincipal_thenReturnListOfBikes() {
@@ -31,6 +33,22 @@ class BikeServiceTest {
         List<Bike> expected = new ArrayList<>(List.of(testBike));
         //THEN
         verify(bikeRepository).findAll();
+        verify(principal).getName();
+        assertEquals(expected, actual);
+    }
+    @Test
+    void addBike_whenBikeRequest_thenSaveBikeToRepo(){
+        //GIVEN
+        when(idService.generateId()).thenReturn("1");
+        when(bikeRepository.save(testBike)).thenReturn(testBike);
+        when(principal.getName()).thenReturn("steven");
+        BikeRequest bikeRequest = new BikeRequest(testBike.modelName(), testBike.mileage(), testBike.components());
+        //WHEN
+        Bike actual = bikeService.addBike(principal, bikeRequest);
+        Bike expected = testBike;
+        //THEN
+        verify(idService).generateId();
+        verify(bikeRepository).save(testBike);
         verify(principal).getName();
         assertEquals(expected, actual);
     }
