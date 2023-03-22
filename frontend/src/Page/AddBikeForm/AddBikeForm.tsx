80c7a02988029b81be3ff97978f49c26153cf6af
johannesthorbergsson <johannes.thorbergsson@hotmail.de>
2023-03-22 14:33:35 +0100

    change key for table components

--- frontend/src/Page/AddBikeForm/AddBikeForm.tsx
+++ frontend/src/Page/AddBikeForm/AddBikeForm.tsx
@@ -91,7 +91,7 @@ export default function AddBikeForm() {
                             <TableBody>
                                 {components.map((component) => (
                                     <TableRow
-                                        key={Math.random().toString(16).slice(2)}
+                                        key={component.category}
                                         sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                     >
                                         <TableCell component="th" scope="row">
