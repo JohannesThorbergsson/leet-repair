762fce71617aaff69fb7018e81ed9374e3e0adae
johannesthorbergsson <johannes.thorbergsson@hotmail.de>
2023-03-22 10:04:42 +0100

    Fix unique keys for component table

--- frontend/src/Page/AddBikeForm/AddBikeForm.tsx
+++ frontend/src/Page/AddBikeForm/AddBikeForm.tsx
@@ -98,7 +98,7 @@ export default function AddBikeForm() {
                                 <TableBody>
                                     {components.map((component) => (
                                         <TableRow
-                                            key={component.type}
+                                            key={Math.random().toString(16).slice(2)}
                                             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                         >
                                             <TableCell component="th" scope="row">
