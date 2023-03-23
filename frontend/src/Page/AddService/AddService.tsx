7e71e755445bc4032cc757410ecfa2292a0a2d84
johannesthorbergsson <johannes.thorbergsson@hotmail.de>
2023-03-23 16:41:07 +0100

    Add AddService.tsx

--- frontend/src/Page/AddService/AddService.tsx
+++ frontend/src/Page/AddService/AddService.tsx
@@ -1,8 +1,10 @@
-import {Box, TextField, Typography} from "@mui/material";
+import {Box, Paper, Table, TableBody, TableCell, TableContainer, TableRow, TextField, Typography} from "@mui/material";
 import React, {ChangeEvent, useState} from "react";
 import ResponsiveAppBar from "../../Component/ResponsiveAppBar/ResponsiveAppBar";
 import EditComponents from "../../Component/EditComponents/EditComponents";
 import useAddBike from "../../Hooks/useAddBike";
+import TableHeadComponentTable from "../../Component/TableHeadComponentTable/TableHeadComponentTable";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export default function AddService() {
     const[description, setDescription] =useState<string>("")
@@ -72,6 +74,39 @@ export default function AddService() {
                         onChange={handleInputDate}
                         sx={{mt: 1}}
                     />
+                    <Typography variant={"subtitle1"} fontWeight={"medium"} sx={{mt: 1}}>Installed Components</Typography>
+                    <TableContainer component={Paper}>
+                        <Table aria-label="simple table">
+                            <TableHeadComponentTable cells={[{cellName:"Component", align: undefined},
+                                {cellName:"Model", align:"left"}, {cellName:"Age (km)", align:"right"},
+                                {cellName:"", align: "right"}]}/>
+                            <TableBody>
+                                {components.map((component) => (
+                                    <TableRow
+                                        key={component.category}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    >
+                                        <TableCell component="th" scope="row">
+                                            {component.category}
+                                        </TableCell>
+                                        <TableCell align="left">{component.type}</TableCell>
+                                        <TableCell align="right">{component.age}</TableCell>
+                                        <TableCell align="right" sx={{
+                                            p: 0,
+                                            width: 20
+                                        }}>
+                                            <DeleteIcon onClick={() =>handleDeleteComponent(component)} sx={{
+                                                alignSelf: 'end',
+                                                cursor: 'pointer',
+                                                color: '#2196f3',
+                                                mr: 1
+                                            }}/>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
                     <EditComponents components={components}
                                     handleDeleteComponent={handleDeleteComponent}
                                     handleSubmitNewComponent={handleSubmitNewComponent}
