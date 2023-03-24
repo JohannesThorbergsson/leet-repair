679bbc09fbe3b374becdacd4b4fc67cc3fd03b36
Johannes Thorbergsson <johannes.thorbergsson@hotmail.de>
2023-03-24 23:43:37 +0100

    Add messages to exceptions

--- backend/src/main/java/com/github/johannesthorbergsson/backend/bikes/NoSuchBikeException.java
+++ backend/src/main/java/com/github/johannesthorbergsson/backend/bikes/NoSuchBikeException.java
@@ -3,4 +3,7 @@ package com.github.johannesthorbergsson.backend.bikes;
 import java.util.NoSuchElementException;
 
 public class NoSuchBikeException extends NoSuchElementException {
+    public NoSuchBikeException() {
+        super("Bike with given id does not exist");
+    }
 }
--- backend/src/main/java/com/github/johannesthorbergsson/backend/bikes/UnauthorizedAccessException.java
+++ backend/src/main/java/com/github/johannesthorbergsson/backend/bikes/UnauthorizedAccessException.java
@@ -3,4 +3,7 @@ package com.github.johannesthorbergsson.backend.bikes;
 import java.util.NoSuchElementException;
 
 public class UnauthorizedAccessException extends NoSuchElementException {
+    public UnauthorizedAccessException() {
+        super("Access unauthorized");
+    }
 }
