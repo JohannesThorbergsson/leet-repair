4eaae9343994b0ac5fe8a276a5a096bb6c87b069
Johannes Thorbergsson <johannes.thorbergsson@hotmail.de>
2023-03-29 00:58:27 +0200

    Add OrderForm

---
+++ frontend/src/Component/OrderForm/OrderForm.tsx
@@ -0,0 +1,74 @@
+import {Autocomplete, Box, Button, TextField} from "@mui/material";
+import React from "react";
+import useOrderForm from "../../Hooks/useOrderForm";
+import {Workshop} from "../../model/Workshop";
+import {Bike} from "../../model/Bike";
+import {ServiceOrder} from "../../model/ServiceOrder";
+
+type OrderFormProps = {
+    workshops: Workshop[]
+    bikes: Bike[]
+    orders: ServiceOrder[]
+    updateOrderList(orders: ServiceOrder[]): void
+}
+export default function OrderForm(props: OrderFormProps) {
+    const {
+        workshop,
+        selectedBike,
+        orderDescription,
+        handleInputComponents,
+        handleInputBike,
+        handleInputDescription,
+        handleSubmitOrder
+    } = useOrderForm(props)
+
+    return (
+        <Box component={"form"} onSubmit={handleSubmitOrder}
+             sx ={{
+                 alignItems: 'center',
+                 marginInline: 2,
+                 p: 1,
+                 boxShadow: 1
+             }}>
+            <Autocomplete
+                disablePortal
+                aria-required={true}
+                id="select-bike"
+                onChange={handleInputBike}
+                options={props.bikes.map(bike => bike.modelName)}
+                sx={{ width: 1 }}
+                renderInput={(params) =>
+                    <TextField required {...params} label="Select Bike" />}
+            />
+            <TextField
+                required
+                multiline
+                onChange={handleInputDescription}
+                id="outlined-required"
+                label="Order Description"
+                fullWidth
+                sx={{mt: 2}}
+            />
+            <Autocomplete
+                sx={{mt: 2, width: 1}}
+                multiple
+                options={workshop?.inventory.map(component => component.category + " " +component.type) || []}
+                onChange={handleInputComponents}
+                id="select-components"
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label="(Optional) Order Components"
+                        placeholder="Components"
+                    />
+                )}
+            />
+            <Button variant = "contained"
+                    sx={{mt: 2}}
+                    type={"submit"}
+                    disabled={orderDescription ==="" || !selectedBike}>
+                Submit Order
+            </Button>
+        </Box>
+    )
+}
--- frontend/src/Page/BookOrderPage/BookOrderPage.tsx
+++ frontend/src/Page/BookOrderPage/BookOrderPage.tsx
@@ -2,11 +2,12 @@ import ResponsiveAppBar from "../../Component/ResponsiveAppBar/ResponsiveAppBar"
 import {useNavigate} from "react-router-dom";
 import {Workshop} from "../../model/Workshop";
 import WorkshopCard from "../../Component/WorkshopCard/WorkshopCard";
-import {Autocomplete, Box, Button, TextField, Typography} from "@mui/material";
+import {Box, Button, Typography} from "@mui/material";
 import React from "react";
 import {Bike} from "../../model/Bike";
 import {ServiceOrder} from "../../model/ServiceOrder";
 import useOrderForm from "../../Hooks/useOrderForm";
+import OrderForm from "../../Component/OrderForm/OrderForm";
 
 type OrderFormProps = {
     workshops: Workshop[]
@@ -16,15 +17,7 @@ type OrderFormProps = {
 }
 export default function BookOrderPage(props: OrderFormProps){
     const navigate = useNavigate()
-    const {
-        workshop,
-        selectedBike,
-        orderDescription,
-        handleInputComponents,
-        handleInputBike,
-        handleInputDescription,
-        handleSubmitOrder
-    } = useOrderForm(props)
+    const {workshop} = useOrderForm(props)
 
     return (
         <>
@@ -39,58 +32,15 @@ export default function BookOrderPage(props: OrderFormProps){
                     Selected workshop:
                 </Typography>
             </Box>
-            {workshop && <WorkshopCard workshop={workshop} displayMode={true}/>}
+            {workshop &&
+                <WorkshopCard workshop={workshop} displayMode={true}/>
+            }
             <Typography variant="h5" component="h6" fontWeight={"medium"} sx={{mt: 1}}>
                 Book Services:
             </Typography>
-            <Box component={"form"} onSubmit={handleSubmitOrder}
-            sx ={{
-                alignItems: 'center',
-                marginInline: 2,
-                p: 1,
-                boxShadow: 1
-            }}>
-                <Autocomplete
-                    disablePortal
-                    aria-required={true}
-                    id="select-bike"
-                    onChange={handleInputBike}
-                    options={props.bikes.map(bike => bike.modelName)}
-                    sx={{ width: 1 }}
-                    renderInput={(params) =>
-                        <TextField required {...params} label="Select Bike" />}
-                />
-                <TextField
-                    required
-                    multiline
-                    onChange={handleInputDescription}
-                    id="outlined-required"
-                    label="Order Description"
-                    fullWidth
-                    sx={{mt: 2}}
-                />
-                <Autocomplete
-                    sx={{mt: 2, width: 1}}
-                    multiple
-                    options={workshop?.inventory.map(component => component.category + " " +component.type) || []}
-                    onChange={handleInputComponents}
-                    id="select-components"
-                    renderInput={(params) => (
-                        <TextField
-                            {...params}
-                            label="(Optional) Order Components"
-                            placeholder="Components"
-                        />
-                    )}
-                />
-                <Button variant = "contained"
-                        sx={{mt: 2}}
-                        type={"submit"}
-                        disabled={orderDescription ==="" || !selectedBike}>
-                    Submit Order
-                </Button>
-            </Box>
-            <Button sx={{mt: 2}} variant={"contained"} onClick={()=>navigate("/")}>Back</Button>
+            <OrderForm workshops={props.workshops} bikes={props.bikes} orders={props.orders}
+                       updateOrderList={props.updateOrderList}/>
+            <Button sx={{mt: 2, width: 92/100}} variant={"contained"} onClick={()=>navigate("/")}>Back</Button>
         </>
     )
 }
