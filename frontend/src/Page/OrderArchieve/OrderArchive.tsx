91ed32f336bb8956bde468c94cc23f23561944fa
Johannes Thorbergsson <johannes.thorbergsson@hotmail.de>
2023-03-30 17:47:53 +0200

    Add OrderArchive

---
+++ frontend/src/Page/OrderArchieve/OrderArchive.tsx
@@ -0,0 +1,37 @@
+import {ServiceOrder} from "../../model/ServiceOrder";
+import ResponsiveAppBar from "../../Component/ResponsiveAppBar/ResponsiveAppBar";
+import {Box, Typography} from "@mui/material";
+import React from "react";
+import Button from "@mui/material/Button";
+import {useNavigate} from "react-router-dom";
+import useAuth from "../../Hooks/useAuth";
+import OrderCard from "../../Component/OrderCard/OrderCard";
+
+type OrderArchiveProps = {
+    orders: ServiceOrder[]
+}
+
+export default function OrderArchive(props: OrderArchiveProps){
+    useAuth(true)
+    const navigate = useNavigate()
+    return (
+        <>
+            <ResponsiveAppBar/>
+            {props.orders.length>0?
+                <Box>
+                    <Typography variant="h4" component="h4" fontWeight={"bold"} sx={{m:2}}>
+                        Completed Orders:
+                    </Typography>
+                    {props.orders.filter(order=> order.status==="DONE").map(order=>
+                        <OrderCard key={order.id} order={order}/>)}
+                </Box>:
+                <Typography variant="h4" component="h4" fontWeight={"bold"} sx={{m:2}}>
+                    No Completed Orders
+                </Typography>
+                }
+            <Button variant={"contained"} sx={{width: 9/10}} onClick={()=> navigate("/")}>
+                Back to Dashboard
+            </Button>
+        </>
+    )
+}
