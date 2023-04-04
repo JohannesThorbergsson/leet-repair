9750135bde2ed7a8849976054fa0910fe7f43370
Johannes Thorbergsson <johannes.thorbergsson@hotmail.de>
2023-04-04 17:07:57 +0200

    Add unit + integration test for addWorkshop

--- backend/src/test/java/com/github/johannesthorbergsson/backend/workshops/WorkshopControllerTest.java
+++ backend/src/test/java/com/github/johannesthorbergsson/backend/workshops/WorkshopControllerTest.java
@@ -1,10 +1,12 @@
 package com.github.johannesthorbergsson.backend.workshops;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.johannesthorbergsson.backend.bikes.Component;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,6 +16,7 @@ import java.util.List;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,10 +29,14 @@ class WorkshopControllerTest {
     MockMvc mockMvc;
     @Autowired
     WorkshopRepository workshopRepository;
+    @Autowired
+    ObjectMapper mapper = new ObjectMapper();
     Component tyre = new Component("tyre", "Pirelli", 1337);
-    Workshop workshop1 = new Workshop("1", "workshop42",
+    Workshop workshop1 = new Workshop("1", "workshop42", "workshop42",
             new ArrayList<>(List.of("tyre", "chain")), List.of(tyre));
-    Workshop workshop2 = new Workshop("2", "workshop1337",
+    WorkshopRequest workshop1Request =
+            new WorkshopRequest(workshop1.name(), workshop1.services(), workshop1.inventory());
+    Workshop workshop2 = new Workshop("1", "workshop1337", "workshop1337",
             new ArrayList<>(List.of("tyre", "brakes")), List.of(tyre));
     @Test
     @DirtiesContext
@@ -70,5 +77,34 @@ class WorkshopControllerTest {
                         ]
                         """)
                         );
+    }
+    @Test
+    @DirtiesContext
+    @WithMockUser(username = "workshop42")
+    void addWorkshop_whenWorkshopRequest_thenReturnWorkshop() throws Exception {
+        //GIVEN
+        String requestJSON = mapper.writeValueAsString(workshop1Request);
+        //WHEN
+        mockMvc.perform(post("/api/workshops/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJSON)
+                .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                        """
+                        {
+                            "name": "workshop42",
+                            "username": "workshop42",
+                            "services": ["tyre", "chain"],
+                            "inventory": [
+                                {
+                                    "category": "tyre",
+                                    "type": "Pirelli",
+                                    "age": 1337
+                                }
+                            ]
+                        }
+                        """));
+
     }
 }
--- backend/src/test/java/com/github/johannesthorbergsson/backend/workshops/WorkshopServiceTest.java
+++ backend/src/test/java/com/github/johannesthorbergsson/backend/workshops/WorkshopServiceTest.java
@@ -1,9 +1,11 @@
 package com.github.johannesthorbergsson.backend.workshops;
 
 import com.github.johannesthorbergsson.backend.bikes.Component;
+import com.github.johannesthorbergsson.backend.id.IdService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,17 +15,21 @@ import static org.mockito.Mockito.when;
 
 class WorkshopServiceTest {
     WorkshopService workshopService;
+    IdService idService = mock(IdService.class);
     WorkshopRepository workshopRepository = mock(WorkshopRepository.class);
+    Principal principal = mock(Principal.class);
     Component tyre = new Component("tyre", "Pirelli", 1337);
-    Workshop workshop1 = new Workshop("1", "workshop42",
+    Workshop workshop1 = new Workshop("1", "workshop42", "workshop42",
             new ArrayList<>(List.of("tyre", "chain")), List.of(tyre));
-    Workshop workshop2 = new Workshop("1", "workshop1337",
+    WorkshopRequest workshop1Request =
+            new WorkshopRequest(workshop1.name(), workshop1.services(), workshop1.inventory());
+    Workshop workshop2 = new Workshop("1", "workshop1337", "workshop1337",
             new ArrayList<>(List.of("tyre", "brakes")), List.of(tyre));
     List<Workshop> expected = new ArrayList<>(List.of(workshop1, workshop2));
 
     @BeforeEach
     void setUp (){
-        workshopService = new WorkshopService(workshopRepository);
+        workshopService = new WorkshopService(workshopRepository, idService);
     }
 
     @Test
@@ -34,5 +40,18 @@ class WorkshopServiceTest {
         List<Workshop> actual = workshopService.getAllWorkshops();
         //THEN
         assertEquals(expected, actual);
+    }
+    @Test
+    void addWorkshop_whenValidWorkshop_thenReturnSavedWorkshop(){
+        //GIVEN
+        when(idService.generateId()).thenReturn("1");
+        when(principal.getName()).thenReturn("workshop42");
+        when(workshopRepository.save(workshop1)).thenReturn(workshop1);
+        //WHEN
+        Workshop expected = workshop1;
+        Workshop actual = workshopService.addWorkshop(principal, workshop1Request);
+        //THEN
+        assertEquals(expected, actual);
+
     }
 }
