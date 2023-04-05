8b5742f39e2e2ce20a2d73bd33d2390295e5cae3
Johannes Thorbergsson <johannes.thorbergsson@hotmail.de>
2023-04-05 12:49:06 +0200

    Add WorkshopResponse DTO

---
+++ backend/src/main/java/com/github/johannesthorbergsson/backend/workshops/WorkshopResponse.java
@@ -0,0 +1,15 @@
+package com.github.johannesthorbergsson.backend.workshops;
+
+import com.github.johannesthorbergsson.backend.bikes.Component;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+public record WorkshopResponse(
+        @Id
+        String id,
+        String name,
+        List<String> services,
+        List<Component> inventory
+) {
+}
