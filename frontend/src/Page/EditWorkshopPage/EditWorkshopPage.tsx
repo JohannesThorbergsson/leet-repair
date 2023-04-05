0f010a0b95d189ed43fcbaca6b99a4f01d197e18
Johannes Thorbergsson <johannes.thorbergsson@hotmail.de>
2023-04-05 12:05:24 +0200

    Add EditWorkshopPage + routes in App.tsx

---
+++ frontend/src/Page/EditWorkshopPage/EditWorkshopPage.tsx
@@ -0,0 +1,24 @@
+import ResponsiveAppBar from "../../Component/ResponsiveAppBar/ResponsiveAppBar";
+import EditWorkshopForm from "../../Component/EditWorkshopForm/EditWorkshopForm";
+import useAuth, {User} from "../../Hooks/useAuth";
+import {Workshop} from "../../model/Workshop";
+import {useParams} from "react-router-dom";
+
+type EditWorkshopPage = {
+    user: User | null
+    workshops: Workshop[]
+    updateWorkshopList(workshops: Workshop[]): void
+}
+export default function EditWorkshopPage(props: EditWorkshopPage){
+    useAuth(true)
+    const {workshopId} = useParams<{workshopId: string}>()
+    const workshop = props.workshops.find(workshop=> workshop.id===workshopId)
+    return (
+        <>
+            <ResponsiveAppBar/>
+            <EditWorkshopForm user={props.user}
+                              workshops={props.workshops}
+                              updateWorkshopList={props.updateWorkshopList}/>
+        </>
+    )
+}
