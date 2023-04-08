d45757813ea3a8774c52ff8e9f43912548c4d90b
JohannesThorbergsson <johannes.thorbergsson@hotmail.de>
2023-04-08 17:25:18 +0200

    Add loading animation

--- frontend/src/Component/LoadingScreen/LoadingScreen.tsx
+++ frontend/src/Component/LoadingScreen/LoadingScreen.tsx
@@ -1,9 +1,12 @@
 import "./LoadingScreen.css";
+import wheel from "./bikeWheel.png"
 
 export default function LoadingScreen() {
     return (
         <div className={"loading-screen"}>
-            <span className={"spinner"}/>
+            <div className={"img-container"}>
+                <img src={wheel} className={"spinner"} alt={"wheel"}/>
+            </div>
         </div>
     );
 }
