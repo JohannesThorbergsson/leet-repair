f20e8247a7b9a955b009db7d441943f01e84f9b5
Johannes Thorbergsson <johannes.thorbergsson@hotmail.de>
2023-04-14 22:36:57 +0200

    refactor

--- frontend/src/Component/InvalidId/InvalidId.tsx
+++ frontend/src/Component/InvalidId/InvalidId.tsx
@@ -8,7 +8,7 @@ export default function InvalidId(){
     return (
             <Box>
                 <ResponsiveAppBar/>
-                <Typography variant="h4" component="h4" fontWeight={"bold"} sx={{m: 3}}>Invalid Bike ID</Typography>
+                <Typography variant="h4" component="h4" fontWeight={"bold"} sx={{m: 3}}>Invalid ID</Typography>
                 <Button variant={"contained"} onClick={() => navigate("/bikes")}>Back</Button>
             </Box>
         )
