eefe2009a6b631209a072bf8dd770c90cb95b541
Johannes Thorbergsson <johannes.thorbergsson@hotmail.de>
2023-04-18 15:57:23 +0200

    refactor

--- backend/src/test/java/com/github/johannesthorbergsson/backend/security/CsrfControllerTest.java
+++ backend/src/test/java/com/github/johannesthorbergsson/backend/security/CsrfControllerTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -18,7 +17,6 @@ class CsrfControllerTest {
     MockMvc mockMvc;
 
     @Test
-    @WithMockUser
     void getCsrf_returnStringAndXSRFInCookie() throws Exception {
         mockMvc.perform(get("/api/csrf"))
                 .andExpect(status().isOk())
